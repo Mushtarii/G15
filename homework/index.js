afc14a578b838e8dbb6d0bdc278e820ff2f9352d
Mushtarii <Mushtariy_Zabieva@student.itpu.uz>
2023-11-03 10:18:40 +0300

    homework for type convertion

---
+++ homework/index.js
@@ -0,0 +1,44 @@
+//? Task 1
+//let a = 5>4;
+//console.log(a); //true
+
+//let a = "apple" > "pineapple";
+//console.log(a); //false
+
+//console.log(undefined == null); //true
+//console.log(undefined === null); //false
+
+//console.log(null == "/n0/n"); //false
+//console.log(null === "/n0/n") //false
+
+//? Task 2
+//let a = 1, b = 1;
+//let c = ++a; //2
+//console.log(c);
+
+//let d = b++;
+//console.log(d); //1
+
+//? Task 3
+//let a = 21;
+//let x = 1 + (a *= 2); //43
+//console.log(x);
+
+
+//? Task 4
+"" + 1 + 0 // 10
+"" - 1 + 0 // 10
+true + false //true
+6 / "3" // number 
+"2" * "3" //number
+4 + 5 + "px" //string
+"$" + 4 + 5 //string
+"4" - 2 // number
+"4px" - 2 // number
+" -9 " + 5 //string
+" -9 " - 5 // number
+null + 1 // 
+undefined + 1 //
+" /t /n" - 2 //
+
+
