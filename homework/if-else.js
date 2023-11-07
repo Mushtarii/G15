78e0e8361142899bb01461a2dad76e9e8cdbc847
Mushtarii <Mushtariy_Zabieva@student.itpu.uz>
2023-11-07 23:17:35 +0300

    homework for if-else

---
+++ homework/if-else.js
@@ -0,0 +1,28 @@
+//? Task1
+// if ("0") {
+//     alert('Hello');
+// } //Hello
+
+//? Task2
+// let answer = prompt("What is the official name of JavaScript?");
+// if (isNaN(+answer)){
+// if (answer === "ECMAScript") {
+//   alert("Right");
+// } else {
+// 	alert("Wrong")
+// 	alert("You don't know? 'ECMAScript'")
+// }
+// } else alert("Use only words!")
+
+
+//? Task3
+// let answer = prompt("Enter number");
+// if (+answer >0) {
+//     alert(1);
+// } else if (+answer <0) {
+//     alert(-1);
+// } else {
+//     alert(0)
+// }
+
+
